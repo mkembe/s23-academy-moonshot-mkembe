657f29bbf360d845d76dfbaf18636e077712c57f
Millie Kembe <mikembe@unc.edu>
2023-03-28 04:01:59 -0400

    Moonshot

---
+++ Moonshot/Moonshot/Astronaut .swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut .swift
+//  Moonshot
+//
+//  Created by Millie Kembe on 3/27/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Millie Kembe on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
